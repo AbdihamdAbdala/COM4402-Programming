ad94c05fdd70d9c58f95a6cb490554a6e765ebfc
AbdihamidAbdala <liljakegraal@gmail.com>
2025-12-16 10:10:54 +0000

    Remove unnecessary semi-colons

--- WK2/IO/Activity3.py
+++ WK2/IO/Activity3.py
@@ -1,5 +1,5 @@
 # Activity 3: Type Confusion
 # Fix is to convert the input to an int type, that way we can perform arithmetic operations
 age = int(input("Enter age:"))
-age_next_year = age + 1;
-print(age_next_year);
+age_next_year = age + 1
+print(age_next_year)
